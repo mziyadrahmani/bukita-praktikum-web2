3cca4ecc5778659dd92c11c8db7893722ec910b0
DreamNinjas <mziyadrgt@gmail.com>
2023-10-25 10:06:18 +0800

    Initial commit

---
+++ app/views/about/index.php
@@ -0,0 +1,53 @@
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Halaman About Me</h1>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="card card-primary">
+        <div class="card-header">
+          <h3 class="card-title">Hello</h3>
+        </div>
+        <div class="card-body box-profile">
+          <div class="text-center">
+            <img class="profile-user-img img-fluid img-circle" src="<?= base_url; ?>/dist/img/redsz.png" alt="User profile picture">
+          </div>
+
+          <h3 class="profile-username text-center">@ziyadrahmani</h3>
+
+          <p class="text-muted text-center">Illustrator</p>
+
+          <ul class="list-group list-group-unbordered mb-3">
+            <li class="list-group-item">
+              <strong><i class="fas fa-map-marker-alt mr-1"></i> Location</strong>
+              <a class="float-right"> Banjarmasin</a>
+            </li>
+            <li class="list-group-item">
+              <strong><i class="fas fa-pencil-alt mr-1"></i> Skills</strong>
+              <a class="float-right"> Illustration, Game Dev</a>
+            </li>
+            <li class="list-group-item">
+              <strong><i class="fas fa-pencil-alt mr-1"></i> Website</strong>
+              <a class="float-right" href="gilacoding.com"> Arstation.com/ziyadrahmani </a>
+            </li>
+          </ul>
+        </div>
+        <!-- /.card-body -->
+      </div>
+      <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
